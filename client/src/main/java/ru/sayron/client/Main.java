dc3df2da11bdbb165b46d74b0b48e35f8cb1a089
Sayronar <89864711+Sayronar@users.noreply.github.com>
2023-05-03 11:51:30 +0300

    Test2
    Test2

--- client/src/main/java/ru/sayron/client/Main.java
+++ client/src/main/java/ru/sayron/client/Main.java
@@ -3,6 +3,6 @@ package ru.sayron.client;
 public class Main {
     public static void main(String[] args) {
         System.out.println("23646576");
+        System.out.println("Easter egg!");
     }
-    System.out.println("Easter egg!");
 }
