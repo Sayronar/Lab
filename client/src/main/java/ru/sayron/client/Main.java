fa3222e3833a2aef2c569ae58a82d39455af2a98
daniilbatmanov <iambatmanov@gmail.com>
2023-05-04 13:08:28 +0300

    added for upper realisation

--- client/src/main/java/ru/sayron/client/Main.java
+++ client/src/main/java/ru/sayron/client/Main.java
@@ -2,7 +2,6 @@ package ru.sayron.client;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("23646576");
+
     }
-    System.out.println("Easter egg!");
 }
