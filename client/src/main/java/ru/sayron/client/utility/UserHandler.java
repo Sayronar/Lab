351b18a2ce730c91fe7caab7afe7b4e82da83c54
daniilbatmanov <iambatmanov@gmail.com>
2023-05-04 14:40:22 +0300

    TODO: fix ALL

---
+++ client/src/main/java/ru/sayron/client/utility/UserHandler.java
@@ -0,0 +1,231 @@
+package ru.sayron.client.utility;
+
+// import ru.sayron.client.Main;
+// import common.data.*;
+// import common.exceptions.CommandUsageException;
+// import common.exceptions.IncorrectInputInScriptException;
+// import common.exceptions.ScriptRecursionException;
+// import common.interaction.OrganizationRaw;
+// import common.interaction.Request;
+// import common.interaction.ResponseCode;
+// import common.utility.Outputer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UserHandler {
+    private final int maxRewriteAttempts = 1;
+
+    private Scanner userScanner;
+    private Stack<File> scriptStack = new Stack<>();
+    private Stack<Scanner> scannerStack = new Stack<>();
+
+    public UserHandler(Scanner userScanner) {
+        this.userScanner = userScanner;
+    }
+
+
+    public Request handle(ResponseCode serverResponseCode) {
+        String userInput;
+        String[] userCommand;
+        ProcessingCode processingCode;
+        int rewriteAttempts = 0;
+        try {
+            do {
+                try {
+                    if (fileMode() && (serverResponseCode == ResponseCode.ERROR ||
+                            serverResponseCode == ResponseCode.SERVER_EXIT))
+                        throw new IncorrectInputInScriptException();
+                    while (fileMode() && !userScanner.hasNextLine()) {
+                        userScanner.close();
+                        userScanner = scannerStack.pop();
+                        Outputer.println("Возвращаюсь к скрипту '" + scriptStack.pop().getName() + "'...");
+                    }
+                    if (fileMode()) {
+                        userInput = userScanner.nextLine();
+                        if (!userInput.isEmpty()) {
+                            Outputer.print(Main.PS1);
+                            Outputer.println(userInput);
+                        }
+                    } else {
+                        Outputer.print(Main.PS1);
+                        userInput = userScanner.nextLine();
+                    }
+                    userCommand = (userInput.trim() + " ").split(" ", 2);
+                    userCommand[1] = userCommand[1].trim();
+                } catch (NoSuchElementException | IllegalStateException exception) {
+                    Outputer.println();
+                    Outputer.printerror("Произошла ошибка при вводе команды!");
+                    userCommand = new String[]{"", ""};
+                    rewriteAttempts++;
+                    if (rewriteAttempts >= maxRewriteAttempts) {
+                        Outputer.printerror("Превышено количество попыток ввода!");
+                        System.exit(0);
+                    }
+                }
+                processingCode = processCommand(userCommand[0], userCommand[1]);
+            } while (processingCode == ProcessingCode.ERROR && !fileMode() || userCommand[0].isEmpty());
+            try {
+                if (fileMode() && (serverResponseCode == ResponseCode.ERROR || processingCode == ProcessingCode.ERROR))
+                    throw new IncorrectInputInScriptException();
+                switch (processingCode) {
+                    case OBJECT:
+                        OrganizationRaw organizationAddRaw = generateOrganizationAdd();
+                        return new Request(userCommand[0], userCommand[1], organizationAddRaw);
+                    case UPDATE_OBJECT:
+                        OrganizationRaw organizationUpdateRaw = generateOrganizationUpdate();
+                        return new Request(userCommand[0], userCommand[1], organizationUpdateRaw);
+                    case SCRIPT:
+                        File scriptFile = new File(userCommand[1]);
+                        if (!scriptFile.exists()) throw new FileNotFoundException();
+                        if (!scriptStack.isEmpty() && scriptStack.search(scriptFile) != -1)
+                            throw new ScriptRecursionException();
+                        scannerStack.push(userScanner);
+                        scriptStack.push(scriptFile);
+                        userScanner = new Scanner(scriptFile);
+                        Outputer.println("Выполняю скрипт '" + scriptFile.getName() + "'...");
+                        break;
+                }
+            } catch (FileNotFoundException exception) {
+                Outputer.printerror("Файл со скриптом не найден!");
+            } catch (ScriptRecursionException exception) {
+                Outputer.printerror("Скрипты не могут вызываться рекурсивно!");
+                throw new IncorrectInputInScriptException();
+            }
+        } catch (IncorrectInputInScriptException exception) {
+            Outputer.printerror("Выполнение скрипта прервано!");
+            while (!scannerStack.isEmpty()) {
+                userScanner.close();
+                userScanner = scannerStack.pop();
+            }
+            scriptStack.clear();
+            return new Request();
+        }
+        return new Request(userCommand[0], userCommand[1]);
+    }
+
+    /**
+     * Processes the entered command.
+     *
+     * @return Status of code.
+     */
+    private ProcessingCode processCommand(String command, String commandArgument) {
+        try {
+            switch (command) {
+                case "":
+                    return ProcessingCode.ERROR;
+                case "help":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "info":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "show":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "add":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException("{element}");
+                    return ProcessingCode.OBJECT;
+                case "update":
+                    if (commandArgument.isEmpty()) throw new CommandUsageException("<ID> {element}");
+                    return ProcessingCode.UPDATE_OBJECT;
+                case "remove_by_id":
+                    if (commandArgument.isEmpty()) throw new CommandUsageException("<ID>");
+                    break;
+                case "clear":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "save":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "execute_script":
+                    if (commandArgument.isEmpty()) throw new CommandUsageException("<file_name>");
+                    return ProcessingCode.SCRIPT;
+                case "exit":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "add_if_min":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException("{element}");
+                    return ProcessingCode.OBJECT;
+                case "remove_greater":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException("{element}");
+                    return ProcessingCode.OBJECT;
+                case "history":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "sum_of_health":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "max_by_melee_weapon":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                case "filter_by_weapon_type":
+                    if (commandArgument.isEmpty()) throw new CommandUsageException("<weapon_type>");
+                    break;
+                case "server_exit":
+                    if (!commandArgument.isEmpty()) throw new CommandUsageException();
+                    break;
+                default:
+                    Outputer.println("Команда '" + command + "' не найдена. Наберите 'help' для справки.");
+                    return ProcessingCode.ERROR;
+            }
+        } catch (CommandUsageException exception) {
+            if (exception.getMessage() != null) command += " " + exception.getMessage();
+            Outputer.println("Использование: '" + command + "'");
+            return ProcessingCode.ERROR;
+        }
+        return ProcessingCode.OK;
+    }
+
+
+    private OrganizationRaw generateOrganizationAdd() throws IncorrectInputInScriptException {
+        OrganizationAsker organizationAsker = new OrganizationAsker(userScanner);
+        if (fileMode()) organizationAsker.setFileMode();
+        return new OrganizationRaw(
+                organizationAsker.askName(),
+                organizationAsker.askCoordinates(),
+                organizationAsker.askHealth(),
+                organizationAsker.askCategory(),
+                organizationAsker.askWeaponType(),
+                organizationAsker.askMeleeWeapon(),
+                organizationAsker.askChapter()
+        );
+    }
+
+
+    private OrganizationRaw generateOrganizationUpdate() throws IncorrectInputInScriptException {
+        OrganizationAsker organizationAsker = new OrganizationAsker(userScanner);
+        if (fileMode()) organizationAsker.setFileMode();
+        String name = organizationAsker.askQuestion("Хотите изменить имя солдата?") ?
+                organizationAsker.askName() : null;
+        Coordinates coordinates = organizationAsker.askQuestion("Хотите изменить координаты солдата?") ?
+                organizationAsker.askCoordinates() : null;
+        double health = organizationAsker.askQuestion("Хотите изменить здоровье солдата?") ?
+                organizationAsker.askHealth() : -1;
+        AstartesCategory category = organizationAsker.askQuestion("Хотите изменить категорию солдата?") ?
+                organizationAsker.askCategory() : null;
+        Weapon weaponType = organizationAsker.askQuestion("Хотите изменить оружие дальнего боя солдата?") ?
+                organizationAsker.askWeaponType() : null;
+        MeleeWeapon meleeWeapon = organizationAsker.askQuestion("Хотите изменить оружие ближнего боя солдата?") ?
+                OrganizationAsker.askMeleeWeapon() : null;
+        Chapter chapter = organizationAsker.askQuestion("Хотите изменить орден солдата?") ?
+                organizationAsker.askChapter() : null;
+        return new OrganizationRaw(
+                name,
+                coordinates,
+                health,
+                category,
+                weaponType,
+                meleeWeapon,
+                chapter
+        );
+    }
+
+
+    private boolean fileMode() {
+        return !scannerStack.isEmpty();
+    }
+}
